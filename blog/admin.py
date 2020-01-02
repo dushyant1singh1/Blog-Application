0c030790c1878a81763d5c192874ae6d96c50a6b
dushyant1singh1 <dushyant1singh1@gmail.com>
2020-01-02 15:10:57 +0530

    customize admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+#admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','slug','author','publish','status')
+    list_filter=('title','body')
+    prepopulated_fields={'slug':('title',)}
+    raw_id_fields=('author',)
+    date_hierarchy='publish'
+    ordering=('status','publish')
+    
