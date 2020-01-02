83c0f67e51db7a63062ac37e388e38c70e81eb6f
dushyant1singh1 <dushyant1singh1@gmail.com>
2020-01-02 16:35:37 +0530

    added custom manager for querryset

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES=(
         ('draft','Draft'),
@@ -22,8 +26,15 @@ class Post(models.Model):
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
     status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+
+    objects=models.Manager()#default manager
+    published=PublishedManager()#custom manager
+    
     class Meta:
         ordering=('-publish',)
     def __str__(self):
         return self.title
-    
+
+
+                                        
+  
