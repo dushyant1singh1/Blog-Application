7c90142569939e29c11e26c341f483a2a355ca73
dushyant1singh1 <dushyant1singh1@gmail.com>
2020-01-11 23:08:55 +0530

    updated models.py file with more custom managers support

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,29 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse #it will allows us to build urls by their name and passing optional parameters
 
+class AllQuerySet(models.QuerySet):
+    def published(self):
+        return self.filter(status='published')
+    def gadgets(self):
+        return self.filter(category='gadgets',status='published')
+    def machinelearning(self):
+        return self.filter(category='machine learning',status='published')
+    def getevents(self):
+        return self.filter(category='events',status='published')
+
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager,self).get_queryset().filter(status='published')
+        return AllQuerySet(self.model, using=self._db)
+    def published(self):
+        return self.get_queryset().published()
+    def gadgets(self):
+        return self.get_queryset().machinelearning()
+    def getevents(self):
+        return self.get_queryset().getevents()
+
+
 
 
 class Post(models.Model):
@@ -14,6 +33,7 @@ class Post(models.Model):
         ('draft','Draft'),
         ('published','Published'),
     )
+    SELECT_CATEGORY_POST=(('gadgets','Gadgets'),('machine learning','Machine Learning'),('events','Events'),('not','Not'))
     title=models.CharField(max_length=250)# it translates into varchar
     slug=models.SlugField(max_length=250,unique_for_date='publish')#this is used for building good looking urls, we have added the 
                         # unique_for_date parameter to this field so that we can build urls for posts using their publish date and slug. 
@@ -30,9 +50,10 @@ class Post(models.Model):
     status=models.CharField(max_length=10
                                 ,choices=STATUS_CHOICES
                                 ,default='draft')
-
+    category=models.CharField(max_length=20,choices=SELECT_CATEGORY_POST,default='not')
     objects=models.Manager()#default manager
     published=PublishedManager()#custom manager
+
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',args=[self.publish.year
